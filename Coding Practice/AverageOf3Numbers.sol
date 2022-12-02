4dc9fc392f8a86b8d6817eaf7244a07fcba38c34
Saqib3g <117062853+Saqib3g@users.noreply.github.com>
2022-12-02 11:16:29 +0530

    Create AverageOf3Numbers.sol

---
+++ Coding Practice/AverageOf3Numbers.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier:GPL-3.0
+pragma solidity >=0.7.0<0.9.0;
+contract Demo{
+function Average(uint n1, uint n2, uint n3) public pure returns(uint){
+    return (n1+n2+n3)/3;
+}
+}
